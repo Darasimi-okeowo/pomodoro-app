b8ca83a5bd2496e828752a78d75077a8bcf87519
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-08-02 11:11:26 +0100

    second-commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Pomodoro-app
+    <div className="pomodoro">
+      <h1 className='message'>Message</h1>
+      <p className='timer'>25:00</p>
     </div>
   );
 }
